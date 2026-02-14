7c7e478c8149a1e1963557b02a2bef7874bbbb98
davparrado <davparrado7@gmail.com>
2026-02-13 21:07:03 -0500

    vFinal

--- programa.js
+++ programa.js
@@ -2,6 +2,14 @@ let url ="http://127.0.0.1:8000/getjson"
 
 let myAPI = url + "?Localidad"
 
+let map = L.map('map').setView([4.65, -74.1], 11);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+let capaArboles = null;
+
 document.getElementById("getjson").addEventListener("click", function() {
 
     let localidadSeleccionada = Number(document.getElementById("Localidad").value)
@@ -11,6 +19,7 @@ document.getElementById("getjson").addEventListener("click", function() {
     document.getElementById("resultado").textContent =
         "Código seleccionado: " + localidadSeleccionada
         descargarGeoJSON(localidadSeleccionada);
+        cargarGeoJSON(localidadSeleccionada);
 })
 
 function descargarGeoJSON(idLocalidad) {
@@ -26,4 +35,25 @@ function descargarGeoJSON(idLocalidad) {
             a.click();
             a.remove();
         })
+}
+function cargarGeoJSON(idLocalidad) {
+
+    fetch("http://127.0.0.1:8000/getjson?LocalidadSeleccionada=${idLocalidad}")
+        .then(response => response.json())
+        .then(data => {
+
+            // Eliminar capa anterior si existe
+            if (capaArboles) {
+                map.removeLayer(capaArboles);
+            }
+
+            capaArboles = L.geoJSON(data, {
+                onEachFeature: function (feature, layer) {
+                    layer.bindPopup("ID Árbol: " + feature.properties.OBJECTID);
+                }
+            }).addTo(map);
+
+            // Ajustar zoom al contenido
+            map.fitBounds(capaArboles.getBounds());
+        });
 }
