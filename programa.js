7d0b92884c7c3a8f8bc09e80a4a616e05aaf5ffd
davparrado <davparrado7@gmail.com>
2026-02-13 19:56:08 -0500

    v1

---
+++ programa.js
@@ -0,0 +1,29 @@
+let url ="http://127.0.0.1:8000/Buscar"
+
+let myAPI = url + "?Localidad"
+
+document.getElementById("Buscar").addEventListener("click", function() {
+
+    let localidadSeleccionada = Number(document.getElementById("Localidad").value)
+
+    console.log("La localidad escogida es:", localidadSeleccionada)
+
+    document.getElementById("resultado").textContent =
+        "Código seleccionado: " + localidadSeleccionada
+        descargarGeoJSON(localidadSeleccionada);
+})
+
+function descargarGeoJSON(idLocalidad) {
+
+    fetch("http://127.0.0.1:8000/buscar?LocalidadSeleccionada=${idLocalidad}")
+        .then(response => response.blob())
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = "arboles.geojson";
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+        })
+}
